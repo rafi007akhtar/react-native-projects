fd1c46cb5c958e4ac86fc2bdb04bcef7cb7ae16f
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-03-23 02:47:18 +0530

    added Texts, Buttons, styles and count functionality to the app

--- goals-tracker/App.tsx
+++ goals-tracker/App.tsx
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.textStyles}>Hello, world.</Text>
+      </View>
+      <Text style={styles.textStyles}>Count: {count}</Text>
+
+      <Button
+        title="Increment Count"
+        onPress={() => setCount((prev) => prev + 1)}
+      ></Button>
+      <Button
+        title="Decrement Count"
+        onPress={() => setCount((prev) => prev - 1)}
+        color="#563b6f"
+      ></Button>
     </View>
   );
 }
@@ -13,8 +27,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyles: {
+    borderWidth: 1,
+    padding: 8,
+    borderColor: "#563b6f",
+    margin: 16,
   },
 });
