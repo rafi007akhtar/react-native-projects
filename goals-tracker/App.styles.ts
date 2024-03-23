7ac3a258b51a37273124619126e08c96b07f3d4b
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-03-23 23:19:35 +0530

    Style changes - moved the stylesheet into its own ts file - exported it from there - imported and used in App component - made the input container and goals container distribute height as 1:4

---
+++ goals-tracker/App.styles.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1, // gives the outermost container 100% height
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    display: "flex", // by default it's already flex
+    flexDirection: "row", // NOTE: the default flex-direction in React Native is "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+    flex: 1,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    borderRadius: 4,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 3,
+  },
+});
+
+export default styles;
--- goals-tracker/App.tsx
+++ goals-tracker/App.tsx
@@ -1,4 +1,5 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
+import styles from "./App.styles";
 
 export default function App() {
   return (
@@ -8,28 +9,9 @@ export default function App() {
         <Button title="Add Goal"></Button>
       </View>
 
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals ...</Text>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    padding: 50,
-  },
-  inputContainer: {
-    display: "flex", // by default it's already flex
-    flexDirection: "row", // NOTE: the default flex-direction in React Native is "column",
-    justifyContent: "space-between",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "80%",
-    marginRight: 8,
-    borderRadius: 4,
-    padding: 8,
-  },
-});
