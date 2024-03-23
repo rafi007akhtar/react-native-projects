ffd990f6fa503a3a8632176bd5789654582d959b
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-03-24 01:30:43 +0530

    Handling dynamic aspects - text input changes - clicking on button to update goals state using jotai atom - rendering dynamic goals list

--- goals-tracker/App.tsx
+++ goals-tracker/App.tsx
@@ -1,16 +1,38 @@
 import { Button, Text, TextInput, View } from "react-native";
 import styles from "./App.styles";
+import { useState } from "react";
+import { useAtom } from "jotai";
+import { goals } from "./global-states";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [allGoals, setAllGoals] = useAtom(goals);
+
+  function goalInputHandler(enteredText: string) {
+    setEnteredGoal(enteredText);
+  }
+
+  function addGoalHandler() {
+    setAllGoals((prev) => prev.concat(enteredGoal));
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal." />
-        <Button title="Add Goal"></Button>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal."
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler}></Button>
       </View>
 
       <View style={styles.goalsContainer}>
-        <Text>List of goals ...</Text>
+        <Text>List of goals:</Text>
+
+        {allGoals.map((goal, ind) => (
+          <Text key={ind}>{goal}</Text>
+        ))}
       </View>
     </View>
   );
