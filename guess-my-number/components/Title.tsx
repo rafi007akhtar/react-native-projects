ba31d4fdaaca41bc65d6e64281c72a95c46b1430
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-03-25 22:28:38 +0530

    minor style change: title font and border colors to white

--- guess-my-number/components/Title.tsx
+++ guess-my-number/components/Title.tsx
@@ -1,6 +1,5 @@
 import { BaseProp } from "../models/baseTypes";
 import { StyleSheet, Text } from "react-native";
-import { colors } from "../utils/constants";
 
 export default function Title(props: BaseProp) {
   return <Text style={styles.title}>{props.children}</Text>;
@@ -11,9 +10,9 @@ const styles = StyleSheet.create({
     fontSize: 24,
     textAlign: "center",
     fontWeight: "bold",
-    color: colors.ACCENT_500,
+    color: "white",
     borderWidth: 2,
-    borderColor: colors.ACCENT_500,
+    borderColor: "white",
     padding: 12,
   },
 });
