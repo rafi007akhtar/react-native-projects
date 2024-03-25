d430ab625e86e6c544177dce32388fc4574518b9
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-03-25 22:27:34 +0530

    added utility function to generate random number

---
+++ guess-my-number/utils/common.utils.ts
@@ -0,0 +1,13 @@
+export function generateRandomBetween(
+  min: number,
+  max: number,
+  exclude: number
+) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
