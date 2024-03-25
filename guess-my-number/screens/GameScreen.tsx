5f2692f7b08cc7ee563b3e79825997c8ee65f69a
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-03-25 23:34:56 +0530

    implemented number guessing logic with guard rails

--- guess-my-number/screens/GameScreen.tsx
+++ guess-my-number/screens/GameScreen.tsx
@@ -1,16 +1,47 @@
 import { useAtom } from "jotai";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import { numberToGuess } from "../global-states";
 import Title from "../components/Title";
 import { generateRandomBetween } from "../utils/common.utils";
 import { useState } from "react";
 import NumberContainer from "../components/NumberContainer";
+import PrimaryButton from "../components/PrimaryButton";
 
 export default function GameScreen() {
-  const [enteredNum] = useAtom(numberToGuess);
+  const [userNumber] = useAtom(numberToGuess);
+  const [range, setRange] = useState([1, 100]);
+  const [min, max] = range;
 
-  const initalGuess = generateRandomBetween(1, 100, +enteredNum);
+  const initalGuess = generateRandomBetween(min, max, +userNumber);
   const [currentGuess, setCurrentGuess] = useState(initalGuess);
+  const [numberIsGuessed, setNumberIsGuessed] = useState(false);
+
+  function updateTheGuess(direction: "+" | "-") {
+    if (
+      (direction === "-" && currentGuess < +userNumber) ||
+      (direction === "+" && currentGuess > +userNumber)
+    ) {
+      Alert.alert("Incorrect Hint", "Please try that again.", [
+        { text: "Okay", style: "cancel" },
+      ]);
+      return;
+    }
+
+    let [min, max] = range;
+    if (direction === "-") {
+      max = currentGuess;
+    } else {
+      min = currentGuess;
+    }
+    setRange([min, max]);
+
+    const updatedGuess = generateRandomBetween(min, max, currentGuess);
+    setCurrentGuess(updatedGuess);
+
+    if (updatedGuess === +userNumber) {
+      setNumberIsGuessed(true);
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -19,8 +50,19 @@ export default function GameScreen() {
 
       <View>
         <Text>Higher or lower?</Text>
-        {/* TODO: buttons for + or - */}
+        {!numberIsGuessed && (
+          <View style={styles.buttonsContainer}>
+            <PrimaryButton onClick={() => updateTheGuess("-")}>
+              LOWER
+            </PrimaryButton>
+            <PrimaryButton onClick={() => updateTheGuess("+")}>
+              HIGHER
+            </PrimaryButton>
+          </View>
+        )}
       </View>
+
+      {numberIsGuessed && <Text>You guessed the number</Text>}
 
       {/* TODO: view for log rounds */}
     </View>
@@ -32,5 +74,8 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     marginTop: 50,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
   },
 });
