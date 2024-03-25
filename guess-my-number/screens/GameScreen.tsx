7bf6c9d941920e7cd0bfd346c3cfceb489e01e1c
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-03-25 06:54:58 +0530

    modified game screen component with title and placeholders

--- guess-my-number/screens/GameScreen.tsx
+++ guess-my-number/screens/GameScreen.tsx
@@ -1,9 +1,30 @@
 import { useAtom } from "jotai";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { numberToGuess } from "../global-states";
+import Title from "../components/Title";
 
 export default function GameScreen() {
   const [enteredNum] = useAtom(numberToGuess);
 
-  return <Text>GameScreen works! Need to guess {enteredNum}</Text>;
+  return (
+    <View style={styles.container}>
+      <Title>Opponent's guess</Title>
+      {/* TODO: add guess here */}
+
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* TODO: buttons for + or - */}
+      </View>
+
+      {/* TODO: view for log rounds */}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    marginTop: 50,
+  },
+});
