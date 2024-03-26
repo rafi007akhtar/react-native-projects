d22f55151a178f232b83b0a0d23c7a6d4f5b1786
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-03-27 01:42:30 +0530

    implemented gameover screen with restart logic

--- guess-my-number/screens/GameOverScreen.tsx
+++ guess-my-number/screens/GameOverScreen.tsx
@@ -1,13 +1,36 @@
 import { useAtom } from "jotai";
-import { Text, StyleSheet, View } from "react-native";
-import { numberToGuess } from "../global-states";
+import { Text, StyleSheet, View, Image } from "react-native";
+import { gamePlayAtom, initState } from "../global-states";
+import Title from "../components/Title";
+import { colors } from "../utils/constants";
+import Bold from "../components/Bold";
+import PrimaryButton from "../components/PrimaryButton";
 
 export default function GameOverScreen() {
-  const [enteredNum] = useAtom(numberToGuess);
+  const [gamePlay, setGamePlay] = useAtom(gamePlayAtom);
 
   return (
     <View style={styles.container}>
-      <Text>You guessed the number! It was {enteredNum}.</Text>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/images/success.png")}
+          style={styles.image}
+        />
+      </View>
+      <Text style={styles.summaryText}>
+        The phone needed{" "}
+        <Bold highlightText={true}>{gamePlay.numberOfGuesses}</Bold> tries to
+        guess the number{" "}
+        <Bold highlightText={true}>{gamePlay.numberToGuess}</Bold>.
+      </Text>
+      <PrimaryButton
+        onClick={() => {
+          setGamePlay(initState);
+        }}
+      >
+        Restart
+      </PrimaryButton>
     </View>
   );
 }
@@ -15,7 +38,26 @@ export default function GameOverScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    marginTop: 50,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderRadius: 150,
+    borderColor: colors.PRIMARY_800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24, // fontSize cascades only inside nested Text components
+    textAlign: "center",
   },
 });
