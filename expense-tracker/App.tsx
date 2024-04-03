302548ed89ace926909958f367d93e1180f809ed
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-04-03 23:40:39 +0530

    Setup up basic navigation for all three screens with Stack and Bottom Navigators. Also: - added fonts to the bottom navigators - added other styles for color, alignment etc to the immediately visible navigators - nested bottom navigator inside of stack navigator

--- expense-tracker/App.tsx
+++ expense-tracker/App.tsx
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import RecentExpenses from "./screens/RecentExpenses";
+import AllExpenses from "./screens/AllExpenses";
+import ManageExpense from "./screens/ManageExpense";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Text } from "react-native";
+import { useFonts } from "expo-font";
+import { globalStyles } from "./constants/styles";
 
-export default function App() {
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function ExpenseOverview() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: {
+          fontFamily: globalStyles.fonts.SANS_REGULAR,
+          alignItems: "center",
+          justifyContent: "center",
+        },
+        headerTitleStyle: {
+          fontFamily: globalStyles.fonts.SANS_REGULAR,
+        },
+        headerTitleAlign: "center",
+        headerStyle: {
+          backgroundColor: globalStyles.colors.primary500,
+        },
+        headerTintColor: "white",
+      }}
+    >
+      <Tab.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          title: "Recent Expenses",
+        }}
+      />
+      <Tab.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{ title: "All Expenses" }}
+      />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "product-sans": require("./assets/fonts/Product-Sans-Regular.ttf"),
+    "product-sans-bold": require("./assets/fonts/Product-Sans-Bold.ttf"),
+  });
+
+  let screen = <Text>Loading ...</Text>;
+
+  if (fontsLoaded) {
+    screen = (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="ExpenseOverview"
+            component={ExpenseOverview}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+  return screen;
+}
