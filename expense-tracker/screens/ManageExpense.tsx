90a845cca0ec9769522202a1323c8fca828148b2
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-04-07 16:44:58 +0530

    Changes in Manage Expense: - setting its open state to false whenever it is closed using useEffect return - setting title to edit or add expense based on id present in param - some basic styling

--- expense-tracker/screens/ManageExpense.tsx
+++ expense-tracker/screens/ManageExpense.tsx
@@ -1,5 +1,42 @@
-import { Text } from "react-native";
+import { useAtom } from "jotai";
+import { StyleSheet, Text, View } from "react-native";
+import { manageExpenseOpenedAtom } from "../state/atoms";
+import { useEffect, useLayoutEffect } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 export default function ManageExpense() {
-  return <Text>Manage Expense</Text>;
+  const [_manageExpenseOpened, setManageExpenseOpened] = useAtom(
+    manageExpenseOpenedAtom
+  );
+  const route = useRoute();
+  const navigation = useNavigation();
+
+  let id = (route.params as any)?.id;
+  const isEditing = !!id;
+
+  setManageExpenseOpened(true);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  useEffect(() => {
+    return () => {
+      setManageExpenseOpened(false);
+    };
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Manage Expense</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+});
