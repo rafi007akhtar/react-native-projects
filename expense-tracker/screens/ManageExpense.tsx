a341a4a46d7a6226a8b9903b5d4a07ebd4b25b5d
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-04-16 11:00:13 +0530

    added a trash icon for edit for, and some buttons to the manage expense component for cancel and update / add

--- expense-tracker/screens/ManageExpense.tsx
+++ expense-tracker/screens/ManageExpense.tsx
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View } from "react-native";
 import { manageExpenseOpenedAtom } from "../state/atoms";
 import { useEffect, useLayoutEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
+import IconButton from "../components/UI/IconButton";
+import { globalStyles } from "../constants/styles";
+import Button from "../components/UI/Button";
 
 export default function ManageExpense() {
   const [_manageExpenseOpened, setManageExpenseOpened] = useAtom(
@@ -27,9 +30,32 @@ export default function ManageExpense() {
     };
   }, []);
 
+  function deleteExpenseHandler() {}
+
+  function cancelHandler() {}
+
+  function confirmHandler() {}
+
   return (
     <View style={styles.container}>
-      <Text>Manage Expense</Text>
+      <View style={styles.buttons}>
+        <Button mode="flat" onPress={cancelHandler} style={styles.button}>
+          Cancel
+        </Button>
+        <Button onPress={confirmHandler} style={styles.button}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            name="delete-sweep"
+            size={36}
+            color={globalStyles.colors.error500}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -37,6 +63,23 @@ export default function ManageExpense() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: globalStyles.colors.primary800,
+    padding: 6,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginTop: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: globalStyles.colors.primary200,
+    alignItems: "center",
   },
 });
