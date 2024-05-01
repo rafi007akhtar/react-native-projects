4139df102d122a30aa0ffdb33a6456281e4f8c2e
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-05-01 22:19:06 +0530

    implemented error handling in forms

--- expense-tracker/components/ExpenseForm/ExpenseForm.tsx
+++ expense-tracker/components/ExpenseForm/ExpenseForm.tsx
@@ -1,41 +1,62 @@
 import { StyleSheet, Text, View } from "react-native";
 import Input from "../UI/Input";
 import { globalStyles } from "../../constants/styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../UI/Button";
-import { Expense, ExpenseDetails } from "../../models/expenses.model";
+import {
+  Expense,
+  ExpenseDetails,
+  ExpenseFormInputs,
+} from "../../models/expenses.model";
 import { formatDate } from "../../utils/dates.util";
+import { validateExpenseForm } from "../../utils/validators.util";
+import { FormErrors } from "../../models/errors.model";
 
 interface ExpenseFormProps {
   onCancel: () => void;
   onSubmit: (expenseData: ExpenseDetails) => void;
   isEditing: boolean;
-  defaultValues: Expense | {};
+  defaultValues: Expense;
 }
 
 export default function ExpenseForm(props: ExpenseFormProps) {
-  const defaultValues = { ...props?.defaultValues } || {};
+  const defaultValues = { ...props?.defaultValues };
   const { amount, date, description } = defaultValues;
 
-  const [formInputs, setFormInputs] = useState({
+  const [formInputs, setFormInputs] = useState<ExpenseFormInputs>({
     amount: amount ? `${amount}` : "",
     date: date ? formatDate(date) : "",
     description: description || "",
   });
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [expenseData, setExpenseData] = useState<ExpenseDetails>();
+
+  const [errors, setErrors] = useState<FormErrors>({});
+  // NOTE: useEffect here is indispensable, because other parts of the application
+  // are likely not having the latest values of the state variables
+  useEffect(() => {
+    if (formSubmitted && !Object.keys(errors).length) {
+      props.onSubmit(expenseData);
+    }
+  }, [formSubmitted, errors, expenseData]);
 
   function updateForm(fieldName: string, fieldVal: string) {
+    setFormSubmitted(false);
     setFormInputs((currVal) => {
       return { ...currVal, [fieldName]: fieldVal };
     });
+    errors[fieldName] = undefined;
   }
 
   function formSubmitHandler() {
-    const expenseData: ExpenseDetails = {
+    const expenseDataObj: ExpenseDetails = {
       amount: +formInputs.amount,
       date: new Date(formInputs.date),
       description: formInputs.description,
     };
-    props.onSubmit(expenseData);
+    setExpenseData(expenseDataObj);
+    setErrors(validateExpenseForm(expenseDataObj));
+    setFormSubmitted(true);
   }
 
   return (
@@ -50,7 +71,10 @@ export default function ExpenseForm(props: ExpenseFormProps) {
           style={styles.fullSpace}
           onChangeText={updateForm.bind(this, "amount")}
           value={formInputs.amount}
+          errorMessage={errors?.amount}
+          error={!!errors?.amount}
         ></Input>
+
         <Input
           labelText="Date"
           placeholder="yyyy-mm-dd"
@@ -59,14 +83,19 @@ export default function ExpenseForm(props: ExpenseFormProps) {
           onChangeText={updateForm.bind(this, "date")}
           value={formInputs.date}
           style={styles.fullSpace}
+          errorMessage={errors?.date}
+          error={!!errors?.date}
         ></Input>
       </View>
+
       <Input
         labelText="Description"
         multiline={true}
         numberOfLines={5}
         onChangeText={updateForm.bind(this, "description")}
         value={formInputs.description}
+        errorMessage={errors?.description}
+        error={!!errors?.description}
       ></Input>
 
       <View style={styles.buttons}>
