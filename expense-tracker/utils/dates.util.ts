32b27aa3993f4cff4009039ba701a39e484d2984
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-05-01 18:09:38 +0530

    modified format date to use toISOString method

--- expense-tracker/utils/dates.util.ts
+++ expense-tracker/utils/dates.util.ts
@@ -1,14 +1,5 @@
-export function formatDate(dateObj: Date) {
-  let date: string | number = dateObj.getDate();
-  if (date < 10) {
-    date = `0${date}`;
-  }
-
-  let month: string | number = dateObj.getMonth() + 1;
-  if (month < 10) {
-    month = `0${month}`;
-  }
-  return `${dateObj.getFullYear()}-${month}-${date}`;
+export function formatDate(dateObj: Date): string {
+  return dateObj.toISOString().slice(0, 10);
 }
 
 export function getDateMinusDays(date: Date, days: number) {
