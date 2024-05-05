08369adecf1ce4be3385e6ba4974fe99ba416394
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-05-05 19:15:10 +0530

    adding error handling to add, update, delete utils

--- expense-tracker/utils/http.utils.ts
+++ expense-tracker/utils/http.utils.ts
@@ -1,16 +1,22 @@
 import { Expense, ExpenseDetails, Expenses } from "../models/expenses.model";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 
 const BACKEND_BASE_URL = "https://rn-http-e64e8-default-rtdb.firebaseio.com/";
 const EXPENSES_PATH = "expenses.json";
+// const ERROR_URL = "https://rn-http-e64e8-default-rtdb.firebaseiocom/";
 
 const expenseURL = `${BACKEND_BASE_URL}/${EXPENSES_PATH}`;
 
 const httpUtils = {
   addExpense: async (expenseDetails: ExpenseDetails) => {
-    const response = await axios.post(expenseURL, expenseDetails);
-    const id = response.data?.name;
-    return id;
+    let id: string, err: any;
+    try {
+      const response = await axios.post(EXPENSES_PATH, expenseDetails);
+      id = response.data?.name;
+    } catch (e) {
+      err = e;
+    }
+    return [id, err];
   },
 
   fetchExpenses: async () => {
@@ -35,11 +41,23 @@ const httpUtils = {
 
   updateExpense: async (id: string, expenseDetails: ExpenseDetails) => {
     const updateExpURL = `${BACKEND_BASE_URL}/expenses/${id}.json`;
-    return await axios.put(updateExpURL, expenseDetails);
+    let err: any;
+    try {
+      await axios.put(updateExpURL, expenseDetails);
+    } catch (e) {
+      err = e;
+    }
+    return [err];
   },
 
   deleteExpense: async (id: string) => {
-    return await axios.delete(`${BACKEND_BASE_URL}/expenses/${id}.json`);
+    let err: any;
+    try {
+      await axios.delete(`${BACKEND_BASE_URL}/expenses/${id}.json`);
+    } catch (e) {
+      err = e;
+    }
+    return [err];
   },
 };
 
