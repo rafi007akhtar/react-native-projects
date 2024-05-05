f91361b9d1e041040e999462e8280bd83ec08456
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-05-05 16:37:42 +0530

    fetching all expenses in recent expense component through http utils method, and setting that in the zustand store state

--- expense-tracker/screens/RecentExpenses.tsx
+++ expense-tracker/screens/RecentExpenses.tsx
@@ -1,9 +1,27 @@
+import { useEffect } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import useStore from "../state/stores";
 import { getDateMinusDays } from "../utils/dates.util";
+import httpUtils from "../utils/http.utils";
 
 export default function RecentExpenses() {
-  const expenses = useStore((state) => state.expenses);
+  const [expenses, setExpenses] = useStore((state) => [
+    state.expenses,
+    state.setExpenses,
+  ]);
+
+  useEffect(() => {
+    async function obtainExpenses() {
+      const [expenses, err] = await httpUtils.fetchExpenses();
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      setExpenses(expenses);
+    }
+    obtainExpenses();
+  }, []);
 
   const today = new Date();
   const dateLastWeek = getDateMinusDays(today, 7);
