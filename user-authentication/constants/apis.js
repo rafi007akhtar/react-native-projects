1801aafe16d6f0a29c9de2bf268676925a3bf92a
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-05-15 00:13:37 +0530

    added new contant file to hold api constants

---
+++ user-authentication/constants/apis.js
@@ -0,0 +1,10 @@
+const apiConstants = {
+  FIREBASE_SIGNUP_URL:
+    "https://identitytoolkit.googleapis.com/v1/accounts:MODE_PLACEHOLDER?key=",
+  SIGNUP_MODE: "signUp",
+  SINGIN_MODE: "signInWithPassword",
+  MODE_PLACEHOLDER: "MODE_PLACEHOLDER",
+};
+
+Object.freeze(apiConstants);
+export { apiConstants };
