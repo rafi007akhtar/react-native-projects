c08823d6ad8c52c892e70b2d98c77e909d9bdd52
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-05-15 00:15:03 +0530

    using login and sign up in their respective screen components

--- user-authentication/screens/LoginScreen.js
+++ user-authentication/screens/LoginScreen.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
+import { loginUser } from "../utils/auth.utils";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
 
 function LoginScreen() {
-  return <AuthContent isLogin="true" />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler(credentials) {
+    const { email, password } = credentials;
+
+    setIsAuthenticating(true);
+    const [response, error] = await loginUser(email, password);
+    setIsAuthenticating(false);
+
+    console.log([response, error]);
+    if (error) {
+      Alert.alert("Something went wrong.", "Please check user id / password.");
+    }
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging you in ..." />;
+  }
+
+  return <AuthContent isLogin="true" onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- user-authentication/screens/SignupScreen.js
+++ user-authentication/screens/SignupScreen.js
@@ -1,7 +1,23 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../utils/auth.utils";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function signupHandler(credentials) {
+    const { email, password } = credentials;
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user ..." />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
