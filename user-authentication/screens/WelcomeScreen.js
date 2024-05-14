4b48dc8bc16e921ac9fc9cb1c2cac2a1b42b77e7
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-05-15 01:19:09 +0530

    getting protected message from firebase db after setting auth rules in the db

--- user-authentication/screens/WelcomeScreen.js
+++ user-authentication/screens/WelcomeScreen.js
@@ -1,10 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import useAuthStore from "../state/stores";
 
 function WelcomeScreen() {
+  const [protectedMessage, setProtectedMessage] = useState("");
+  const [token] = useAuthStore((state) => [state.token]);
+
+  useEffect(() => {
+    const messageURL = `https://rn-http-e64e8-default-rtdb.firebaseio.com/message.json?auth=${token}`;
+    axios
+      .get(messageURL)
+      .then((res) => {
+        setProtectedMessage(res?.data);
+      })
+      .catch((err) => {
+        console.log({ err });
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{protectedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +33,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
