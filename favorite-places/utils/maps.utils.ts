87d17d0b4d2910ea127bcff97e7bd523eb9f47c4
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-05-18 00:07:31 +0530

    implemented getAddress from lat / lng coords from google maps

--- favorite-places/utils/maps.utils.ts
+++ favorite-places/utils/maps.utils.ts
@@ -4,10 +4,35 @@ import { privateConstants } from "../constants/privateConstants";
 const GOOGLE_MAPS_BASE_URL =
   "https://maps.googleapis.com/maps/api/staticmap?center=#LAT,#LNG&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C#LAT,#LNG&key=#API_KEY";
 
+const GOOGLE_MAPS_GEOCODING_URL =
+  "https://maps.googleapis.com/maps/api/geocode/json?latlng=#LAT,#LNG&key=#API_KEY";
+
 export function getMapURL(lat: string, lng: string) {
   const mapURL = GOOGLE_MAPS_BASE_URL.replaceAll("#LAT", lat)
     .replaceAll("#LNG", lng)
     .replace("#API_KEY", privateConstants.GOOGLE_MAPS_API_KEY);
   console.log({ mapURL });
   return mapURL;
+}
+
+export async function getAddress(
+  lat: string,
+  lng: string
+): Promise<Array<string | unknown>> {
+  const revGeocodURL = GOOGLE_MAPS_GEOCODING_URL.replace("#LAT", lat)
+    .replace("#LNG", lng)
+    .replace("#API_KEY", privateConstants.GOOGLE_MAPS_API_KEY);
+  console.log({ revGeocodURL });
+
+  let address, error;
+  try {
+    const response = await fetch(revGeocodURL);
+    const result = await response.json();
+    console.log({ result });
+    const formattedAddress: string = result.results[0].formatted_address;
+    address = formattedAddress;
+  } catch (e) {
+    error = e;
+  }
+  return [address, error];
 }
