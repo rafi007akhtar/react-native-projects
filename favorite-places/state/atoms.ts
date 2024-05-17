afda14ce96b7c9074d45e21ef0ee8c93a313c5a8
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-05-18 00:08:22 +0530

    added atoms for global state related to places

--- favorite-places/state/atoms.ts
+++ favorite-places/state/atoms.ts
@@ -1,11 +1,18 @@
 import { atom } from "jotai";
+import { Places } from "../models/place.model";
 
 interface SelectedLocation {
   latitude: number;
   longitude: number;
 }
 
-export const selectedLocationAtom = atom<SelectedLocation>({
+export const defaultLocation = {
   latitude: 18.558512022770415,
   longitude: 73.74953057616949,
-});
+};
+
+export const selectedLocationAtom = atom<SelectedLocation>(defaultLocation);
+export const selectedImageAtom = atom("");
+export const titleAtom = atom("");
+
+export const placesAtom = atom<Places>([]);
