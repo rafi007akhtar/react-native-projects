f37911d03fc6aaba9cbd120f2b1ce6a6ec42fbdd
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-05-18 00:18:47 +0530

    showing all places with atoms in the AllPlaces component

--- favorite-places/screens/AllPlaces.tsx
+++ favorite-places/screens/AllPlaces.tsx
@@ -1,5 +1,8 @@
+import { useAtom } from "jotai";
 import PlacesList from "../components/Places/PlacesList";
+import { placesAtom } from "../state/atoms";
 
 export default function AllPlaces() {
-  return <PlacesList places={[]} />;
+  const [places] = useAtom(placesAtom);
+  return <PlacesList places={places} />;
 }
