1550f49c176e538bac1a152427151c658c1e11ac
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-05-18 19:04:19 +0530

    added readonly mode to map where the sent location is shown

--- favorite-places/screens/Map.tsx
+++ favorite-places/screens/Map.tsx
@@ -1,26 +1,38 @@
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 import { Alert, StyleSheet } from "react-native";
 import MapView, { Region, Marker, MapPressEvent } from "react-native-maps";
 import IconButton from "../components/UI/IconButton";
 import { useAtom } from "jotai";
-import { selectedLocationAtom } from "../state/atoms";
+import { SelectedLocation, selectedLocationAtom } from "../state/atoms";
 
-export default function Map(props: { navigation: any }) {
+export default function Map(props: { navigation: any; route: any }) {
   const [selectedLocation, setSelectedLocation] = useAtom(selectedLocationAtom);
+  let locationToShow = { ...selectedLocation };
+  let readOnlyMap = false;
+
+  const location: SelectedLocation = props?.route?.params?.location;
+  if (location) {
+    locationToShow = { ...location };
+    readOnlyMap = true;
+  }
 
   const region: Region = {
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
-    ...selectedLocation,
+    ...locationToShow,
   };
 
   function mapPressHandler(evt: MapPressEvent) {
+    if (readOnlyMap) {
+      return;
+    }
+
     const { latitude, longitude } = evt.nativeEvent.coordinate;
     setSelectedLocation({ latitude, longitude });
   }
 
   function saveSelectedCoordHandler() {
-    if (!selectedLocation.latitude) {
+    if (!locationToShow.latitude) {
       Alert.alert(
         "Unable to save coordinates",
         "Please select a point in the map before trying to save it."
@@ -30,7 +42,12 @@ export default function Map(props: { navigation: any }) {
 
     props.navigation.navigate("AddPlace");
   }
+
   useLayoutEffect(() => {
+    if (readOnlyMap) {
+      return;
+    }
+
     props.navigation.setOptions({
       headerRight: ({ tintColor }: { tintColor: string }) => (
         <IconButton
@@ -41,7 +58,7 @@ export default function Map(props: { navigation: any }) {
         />
       ),
     });
-  }, [props.navigation, selectedLocation]);
+  }, [props.navigation]);
 
   return (
     <MapView
@@ -49,8 +66,8 @@ export default function Map(props: { navigation: any }) {
       style={styles.map}
       onPress={mapPressHandler}
     >
-      {selectedLocation && (
-        <Marker coordinate={selectedLocation} title="Picked Location" />
+      {locationToShow && (
+        <Marker coordinate={locationToShow} title="Picked Location" />
       )}
     </MapView>
   );
