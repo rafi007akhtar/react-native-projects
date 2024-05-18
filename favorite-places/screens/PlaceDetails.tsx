b2f126cecab9873ffebd75e6e1cd04ab6fb367f7
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-05-18 18:01:35 +0530

    implemented place details component to show place details when clicked

---
+++ favorite-places/screens/PlaceDetails.tsx
@@ -0,0 +1,55 @@
+import { ScrollView, Image, View, Text, StyleSheet } from "react-native";
+import OutlinedButton from "../components/UI/OutlinedButton";
+import { Place } from "../models/place.model";
+import { COLORS } from "../constants/colors";
+import { useLayoutEffect } from "react";
+
+export default function PlacesDetails(props: any) {
+  const params = props?.route?.params;
+  const place: Place = params?.place;
+
+  useLayoutEffect(() => {
+    const navigation = props?.navigation;
+    navigation?.setOptions({
+      headerTitle: place.title,
+    });
+  }, [place.title]);
+
+  function showOnMapHandler() {}
+
+  return (
+    <ScrollView>
+      <Image source={{ uri: place.imageUri }} style={styles.image} />
+      <View style={styles.locationContainer}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}>{place.address}</Text>
+        </View>
+        <OutlinedButton icon="map" onPress={showOnMapHandler}>
+          View on Map
+        </OutlinedButton>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    height: "35%",
+    width: "100%",
+    minHeight: 300,
+    marginBottom: 12,
+  },
+  locationContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  addressContainer: {
+    padding: 20,
+  },
+  address: {
+    color: COLORS.primary500,
+    fontFamily: "product-sans",
+    textAlign: "center",
+    fontSize: 16,
+  },
+});
